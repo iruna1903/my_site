28da67a77483b217b338b9dba7fc7e5fd043d8eb
iruna1903 <irkaduma@gmail.com>
2023-08-30 15:02:51 +0300

    my_site

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,4 @@
-from datetime import date
-
 from django.shortcuts import render
-
-all_posts = [
-    {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpq",
-    "author": "Iryna",
-    "date": date(2023, 8, 25),
-    "title": "Mountain Hiking",
-    "excerpt": " There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-    "content": "The aroma of coffee wafted from the cafe, enticing passersby to step in for a warm cup on a chilly morning."
-    }
-]
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 
